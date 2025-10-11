2f1caf80fe2b91db1f80a6b7be85bc84663d8bfb
PhengPheakdeyP <pheng.pheakdey.1223@rupp.edu.kh>
2025-10-11 19:10:34 +0700

    update new

--- lib/views/categories.dart
+++ lib/views/categories.dart
@@ -282,7 +282,7 @@ class Categories extends StatelessWidget {
                                     Container(
                                       padding: EdgeInsets.all(4),
                                       child: Text(
-                                        "Hello",
+                                        "Hello Sister",
                                         style: TextStyle(
                                           color: const Color.fromARGB(
                                             226,
