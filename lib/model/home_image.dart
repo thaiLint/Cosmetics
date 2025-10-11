8fed03b6a2a6d64efb7053f825137a76406b38c4
sreyheng <ponsreyheng7@gmail.com>
2025-10-11 15:28:33 +0700

    update new

--- lib/model/home_image.dart
+++ lib/model/home_image.dart
@@ -1,16 +1,16 @@
 class product {
   String img;
   String cream;
-  List<String> images;
+  String maintitle;
   String title;
   String describe;
   String discount;
   product({
     required this.img,
     required this.cream,
+    required this.maintitle,
     required this.title,
     required this.describe,
-    required this.images,
     required this.discount,
   });
 }
@@ -19,51 +19,43 @@ List<product> detail = [
   product(
     img: "assets/images/1.png",
     title: "Retinol Youth Renewal Night Cream",
+    maintitle: "Murad",
     cream: "assets/images/cream.png",
     describe:
         "An advanced night cream formulated with powerful retinol . ",
     discount: "20% | OFF BUY NOW",
-    images: [
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-1.jpg?v=1685000000",
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-2.jpg?v=1685000000",
-    ],
+    
   ),
   product(
-    img:"assets/images/1.png",
+    img:"assets/images/2.png",
+    maintitle: "Murad",
     title: "Retinol Youth Renewal Night Cream",
     cream: "assets/images/cream.png",
     describe:
-        "An advanced night cream formulated with powerful retinol and botanical extracts to reduce fine lines, improve skin texture, and enhance radiance. ",
-    discount: "",
-    images: [
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-1.jpg?v=1685000000",
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-2.jpg?v=1685000000",
-    ],
+        "An advanced night cream formulated with powerful retinol . ",
+    discount: "20% | OFF BUY NOW",
+    
   ),
   product(
     img:
-        "https://imageskincare.com/cdn/shop/products/ORMEDIC_balancing_gel_masque_PDP_R01a.jpg?v=1679333767&width=600",
+        "assets/images/4.png",
     title: "Retinol Youth Renewal Night Cream",
     cream: "assets/images/cream.png",
+    maintitle: "Murad",
     describe:
-        "An advanced night cream formulated with powerful retinol and botanical extracts to reduce fine lines, improve skin texture, and enhance radiance. It deeply nourishes while supporting natural collagen production for smoother, firmer, and more youthful-looking skin.",
-    discount: "",
-    images: [
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-1.jpg?v=1685000000",
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-2.jpg?v=1685000000",
-    ],
+        "An advanced night cream formulated with powerful retinol . ",
+    discount: "20% | OFF BUY NOW",
+    
   ),
   product(
     img:
-        "https://imageskincare.com/cdn/shop/products/ORMEDIC_balancing_gel_masque_PDP_R01a.jpg?v=1679333767&width=600",
+        "assets/images/5.png",
     title: "Retinol Youth Renewal Night Cream",
     cream: "assets/images/cream.png",
+    maintitle: "Murad",
     describe:
-        "An advanced night cream formulated with powerful retinol and botanical extracts to reduce fine lines, improve skin texture, and enhance radiance. It deeply nourishes while supporting natural collagen production for smoother, firmer, and more youthful-looking skin.",
-    discount: "",
-    images: [
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-1.jpg?v=1685000000",
-      "https://cdn.shopify.com/s/files/1/0266/6276/4597/products/retinol-youth-renewal-night-cream-2.jpg?v=1685000000",
-    ],
+        "An advanced night cream formulated with powerful retinol . ",
+    discount: "20% | OFF BUY NOW",
+    
   ),
 ];
