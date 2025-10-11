1205ee98b8ba559c9ec72d80388dc6de8aa6dc52
PhengPheakdeyP <pheng.pheakdey.1223@rupp.edu.kh>
2025-10-11 19:35:05 +0700

    up to date

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,10 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-<<<<<<< HEAD
-=======
   file_selector_linux
->>>>>>> Lineth
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,20 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
-<<<<<<< HEAD
-=======
 import file_selector_macos
->>>>>>> Lineth
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-<<<<<<< HEAD
-=======
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
->>>>>>> Lineth
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
