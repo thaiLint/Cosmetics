909f55f0447e8622061f58c364df9bab963596e4
PHEAKDEY <pheakdey@PHEAKDEYs-MacBook-Pro.local>
2025-10-17 22:15:16 +0700

    upadate screen

--- lib/views/dataCategories/screen_type.dart
+++ lib/views/dataCategories/screen_type.dart
@@ -11,55 +11,108 @@ class ScreenType extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text("Zendo"),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          childAspectRatio: 8 / 10,
-        ),
-        itemCount: lisrtData.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Image.asset(
-                    lisrtData[index].type,
-                    height: 150,
-                    width: 150,
-                  ),
-                  Text(lisrtData[index].name),
-                  Spacer(),
-                  Text("\$${lisrtData[index].price}"),
-                  Row(
+      body: Container(
+        width: double.infinity,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 8 / 10,
+          ),
+          itemCount: lisrtData.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Container(
+                width: 170,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 6,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                            color: Colors.deepOrangeAccent,
-                            borderRadius: BorderRadius.circular(5)),
-                        margin: EdgeInsets.only(left: 150),
-                        child: Center(
-                            child: Center(
-                                child: Text(
-                          "+",
-                          style: TextStyle(fontSize: 20),
-                        ))),
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(20),
+                        ),
+                        child: Image.asset(
+                          lisrtData[index].type,
+                          height: 140,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                          vertical: 6,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              lisrtData[index].name,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "\$${lisrtData[index].price.toStringAsFixed(2)}" +
+                                        "USD",
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  Spacer(),
+                                  //link to detail page
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      width: 30,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          20,
+                                        ),
+                                        color: const Color.fromARGB(
+                                          255,
+                                          0,
+                                          0,
+                                          0,
+                                        ),
+                                      ),
+                                      child: Icon(
+                                        Icons.add,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
