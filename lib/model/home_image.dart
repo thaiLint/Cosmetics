501de90e7ef66aa1e1d4356dbbe8115a751ede5d
Thai Lineth <thailinet08@gmail.com>
2025-10-21 02:09:03 +0700

    Restore full code from 38966db (recovery)

--- lib/model/home_image.dart
+++ lib/model/home_image.dart
@@ -59,31 +59,3 @@ List<product> detail = [
     
   ),
 ];
-
-// class Product {
-//   final int id;
-//   final String name;
-//   final int qty;
-//   final double price;
-//   final String image;
-
-//   Product({
-//     required this.id,
-//     required this.name,
-//     required this.qty,
-//     required this.price,
-//     required this.image,
-//   });
-
-//   factory Product.fromJson(Map<String, dynamic> json) {
-//     return Product(
-//       id: json['id'],
-//       name: json['name'],
-//       qty: (json['qty'] as num).toInt(),
-//       price: (json['price'] as num).toDouble(),
-//       image: json['image'],
-//     );
-//   }
-// }
-
-
